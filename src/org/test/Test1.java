061d6f077b56cf2359be00043a02e57daa29dd0e
babyrajendran <babyrajendran1994@gmail.com>
2018-03-23 07:33:48 +0530

    Demoqa Launch

---
+++ src/org/test/Test1.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class Test1 {
+	public static void main(String[] args) {
+		testCase();
+	}
+
+	public static void testCase() {
+		System.out.println("testcase1");
+	}
+}
